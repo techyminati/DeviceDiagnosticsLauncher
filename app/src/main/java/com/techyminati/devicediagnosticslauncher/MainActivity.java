80b014613ecfeab1b5ae799bdc86804c8ea5f335
techyminati <techyminati@outlook.com>
2024-10-27 15:36:42 +0530

    DiagnosticLauncher: Launch activity immediately
    Signed-off-by: techyminati <techyminati@outlook.com>

--- app/src/main/java/com/techyminati/devicediagnosticslauncher/MainActivity.java
+++ app/src/main/java/com/techyminati/devicediagnosticslauncher/MainActivity.java
@@ -1,24 +1,28 @@
 package com.techyminati.devicediagnosticslauncher;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        launchDiagnosticsApp();
+
+        // Optionally close the launcher app after launching the diagnostics app
+        finish();
+    }
+
+    private void launchDiagnosticsApp() {
+        Intent intent = new Intent();
+        intent.setComponent(new ComponentName("com.android.devicediagnostics", "com.android.devicediagnostics.MainActivity"));
+        try {
+            startActivity(intent);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
